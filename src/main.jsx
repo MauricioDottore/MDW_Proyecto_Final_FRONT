7aedfbad9b6e2f6953ef378cc57709cc376ff4f5
Mauricio Dottore <mauricio.dottore@alumnos.uai.edu.ar>
2026-02-26 21:55:03 -0300

    se añadió redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { store } from './redux/store.js'
+import { store, persistor } from './redux/store.js'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'; 
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // 1. El Provider envuelve TODO para dar acceso al store
   <Provider store={store}>
-    <App />
+    {/* 2. El PersistGate espera a que los datos se carguen antes de mostrar la App */}
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
-)
+);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist'; // Importamos persistStore
+import storage from 'redux-persist/es/storage';
+
+const rootReducer = combineReducers({
+    user: userReducer,
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-})
+    reducer: persistedReducer,
+    // El middleware es clave para que Redux Persist no lance errores en consola
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+});
+
+// Ahora sí, exportamos el persistor correctamente
+export const persistor = persistStore(store);
